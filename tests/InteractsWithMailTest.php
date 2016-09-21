8a39af56553291e664882ac45cbf1c046cbbe8eb
Benson Lee <benson.lee@gmail.com>
2016-09-21 08:56:44 -0700

    Add failing test for `seeMessageFrom` when using `alwaysFrom` with both email and name

--- tests/InteractsWithMailTest.php
+++ tests/InteractsWithMailTest.php
@@ -76,4 +76,17 @@ class InteractsWithMailTest extends TestCase
         $this->seeHeaders('X-MailThief-Variables');
         $this->seeHeaders('X-MailThief-Variables', json_encode(['mailthief_id' => 2]));
     }
+
+    public function test_global_from()
+    {
+        $mailer = $this->mailer = $this->getMailThief();
+
+        $mailer->alwaysFrom(['me@example.com' => 'Example Person']);
+
+        $mailer->send('example-view', [], function ($m) {
+            $m->to('john@example.com');
+        });
+
+        $this->seeMessageFrom('me@example.com');
+    }
 }
