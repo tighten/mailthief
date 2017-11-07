3100cbbf3913991988e735bf2c070787ca52dc4d
Freek Van der Herten <freek@spatie.be>
2017-11-07 19:47:10 +0100

    Fix typo

--- src/MailThiefFiveFiveCompatible.php
+++ src/MailThiefFiveFiveCompatible.php
@@ -10,36 +10,36 @@ class MailThiefFiveFiveCompatible extends MailThief implements Mailer, MailQueue
 {
     public function onQueue($queue, $view, array $data, $callback)
     {
-        throw new Exception('MailTheif doesn\'t support queueing in Laravel 5.5');
+        throw new Exception('MailThief doesn\'t support queueing in Laravel 5.5');
     }
 
     public function queueOn($queue, $view, array $data, $callback)
     {
-        throw new Exception('MailTheif doesn\'t support queueing in Laravel 5.5');
+        throw new Exception('MailThief doesn\'t support queueing in Laravel 5.5');
     }
 
     public function laterOn($queue, $delay, $view, array $data, $callback)
     {
-        throw new Exception('MailTheif doesn\'t support queueing in Laravel 5.5');
+        throw new Exception('MailThief doesn\'t support queueing in Laravel 5.5');
     }
 
     public function queue($view, $queue = null)
     {
-        throw new Exception('MailTheif doesn\'t support queueing in Laravel 5.5');
+        throw new Exception('MailThief doesn\'t support queueing in Laravel 5.5');
     }
 
     public function later($delay, $view, $queue = null)
     {
-        throw new Exception('MailTheif doesn\'t support queueing in Laravel 5.5');
+        throw new Exception('MailThief doesn\'t support queueing in Laravel 5.5');
     }
 
     public function to($users)
     {
-        throw new Exception('MailTheif doesn\'t support mailables');
+        throw new Exception('MailThief doesn\'t support mailables');
     }
 
     public function bcc($users)
     {
-        throw new Exception('MailTheif doesn\'t support mailables');
+        throw new Exception('MailThief doesn\'t support mailables');
     }
 }
